fe657532c81a2fefddf2085d8887c7b675fe1660
Jessica <duellje@oregonstate.edu>
2020-10-12 13:53:36 -0400

    refactored code to have constant names in the code to make it more readable

--- 1_guided.py
+++ 1_guided.py
@@ -3,16 +3,26 @@ Emulator
 '''
 import sys
 
+#symbolic names
+PRINT_BEEJ = 1
+HALT = 2
+SAVE_REG = 3
+PRINT_REG = 4
+
 #holds bytes 
 memory = [
     1, #PRINT_BEEJ
     3, #SAVE_REG R1, 37 --> r[1] = 37
     1, #register number
-    37, #value 
+    37,#value 
     4, #PRINT_REG R1 --> print(r[1])
+    1, #register 1
     1, #PRINT_BEEJ
     2  #HALT
 ]
+
+#2 byte 3 instructions 
+# have more than 1 byte in the whole instruction 
 
 #registers 
 #preset names: R0, R1, R2, R3 ... R7
@@ -26,21 +36,22 @@ halted = False #flag check if program is halted
 
 while not halted:
     instruction = memory[pc]
-    if instruction == 1 : #PRINT_BEEJ
+    if instruction == PRINT_BEEJ : #PRINT_BEEJ
         print("Beej!")
         #move pc to next instruction
         pc += 1
-    elif instruction == 2: #HALT
+    elif instruction == HALT: #HALT
         halted = True
         pc += 1
-    elif instruction == 3: #SAVE_REG
+    elif instruction == SAVE_REG: #SAVE_REG
         reg_num = memory[pc + 1]
         value = memory[pc + 2]
         register[reg_num] = value
         pc += 3
-    elif instruction == 4: #PRINT_REG
-        print(register)
-        pc += 1
+    elif instruction == PRINT_REG: #PRINT_REG
+        reg_num = memory[pc + 1]
+        print(register[reg_num])
+        pc += 2
     else:
         print(f"Unknown instruction {instruction}")
         sys.exit(1)
