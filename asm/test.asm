4a45078c2457b90774f96c0a6de2b154473edbf8
Jessica <duellje@oregonstate.edu>
2020-10-19 11:08:46 -0400

    working on jle

--- asm/test.asm
+++ asm/test.asm
@@ -16,6 +16,9 @@
 ; 1
 ; 2
 ; 2
+; 0
+; 0
+; 2
 
 ;MAIN
     LDI R0, 1
@@ -24,6 +27,14 @@
     CALL R2
     LDI R2, CheckJGE
     CALL R2
+    LDI R2, CheckJGT
+    CALL R2
+    LDI R2, CheckJLE
+    CALL R2
+    LDI R0, 4
+    LDI R1, 2
+    CMP R1, R0
+    JLE R2
     HLT
 
 CheckJEQ:
@@ -39,12 +50,41 @@ CheckJGE:
     JGE R2
     RET
 
+CheckJGT:
+    LDI R2, And
+    CMP R0, R1
+    JGT R2
+    RET
+
+CheckJLE:
+    LDI R2, Dec
+    CMP R0, R1
+    JLE R2
+    LDI R2, Div
+    RET
+
 Add:
     ADD R0, R1
     PRN R0 
+    RET
+
+And:
+    AND R0, R1
+    PRN R0
+    RET
+
+Dec:
+    DEC R1
+    PRN R1
+    RET
+
+Div:
+    DIV R0, R1
+    PRN R0
     RET
 
 Mul:
     MUL R0, R1
     PRN R0
     RET
+
